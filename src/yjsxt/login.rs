0e45469bad31b466f3a1c4ae38ebee401f60f7ff
oi <246788426+oi-itsme@users.noreply.github.com>
2026-05-08 15:06:25 +0800

    refactor: 删除yjsxt鉴权用不到的headers缓存

--- src/yjsxt/login.rs
+++ src/yjsxt/login.rs
@@ -1,12 +1,9 @@
 use crate::{
     cas::login::{AccountIssue, CasToken},
-    error::{MapNetworkErr, MapParseErr, MapUnexpectedErr, parse_err},
-    utils::{client, request::cookie_parser},
+    error::{MapNetworkErr, MapUnexpectedErr, parse_err},
+    utils::client,
 };
-use reqwest::{
-    StatusCode,
-    header::{COOKIE, HeaderMap, LOCATION, SET_COOKIE},
-};
+use reqwest::{StatusCode, header::LOCATION};
 
 const YJSXT_FROM_CAS_URL: &str =
     "http://cas.hnu.edu.cn/cas/login?service=http://yjsxt.hnu.edu.cn/gmis/oauthLogin/hndxnew?ywdm=";
@@ -14,7 +11,6 @@ const YJSXT_FROM_CAS_URL: &str =
 /// 研究生系统的令牌
 #[derive(Debug, Clone)]
 pub struct YjsxtToken {
-    headers: HeaderMap,
     id: String,
 }
 
@@ -65,48 +61,15 @@ impl YjsxtToken {
             .ok_or(parse_err(redirection))?
             .to_string();
         let new_url = format!("http://yjsxt.hnu.edu.cn{}", redirection);
-        let cookies: String = cookie_parser(
-            client
-                .get(&new_url)
-                .send()
-                .await
-                .network_err()?
-                .error_for_status()
-                .unexpected_err()?
-                .headers()
-                .get_all(SET_COOKIE),
-        )
-        .join("; ");
-        if cookies.is_empty() {
-            return Err("获取研究生系统 cookie 失败").unexpected_err();
-        }
-        let mut headers = HeaderMap::new();
-        headers.insert(COOKIE, cookies.parse().parse_err(&cookies)?);
-        Ok(Self { headers, id })
+        client
+            .get(&new_url)
+            .send()
+            .await
+            .network_err()?
+            .error_for_status()
+            .unexpected_err()?;
+        Ok(Self { id })
     }
-    /// 从 [HeaderMap] 和 id 创建 [YjsxtToken]
-    ///
-    /// # Parameters
-    ///
-    /// - `headers`: 一个合法的可用作 [YjsxtToken] 的 [HeaderMap]
-    /// - `id`: 研究生系统路径中的 id 标识
-    ///
-    /// # Returns
-    ///
-    /// 返回一个 [YjsxtToken] 实例
-    ///
-    /// # Preconditions
-    ///
-    /// - `headers` 参数应该是一个合法的可用作 [YjsxtToken] 的 [HeaderMap]，否则会导致未定义行为
-    /// - `id` 参数应该是一个合法的可用作 [YjsxtToken] 的 id，否则会导致未定义行为
-    pub fn from_unchecked(headers: HeaderMap, id: String) -> Self {
-        Self { headers, id }
-    }
-    /// 获取当前令牌的 [HeaderMap]，可用于 [YjsxtToken::from_unchecked]
-    pub fn headers(&self) -> &HeaderMap {
-        &self.headers
-    }
-    /// 获取当前令牌的 id，可用于 [YjsxtToken::from_unchecked]
     pub fn id(&self) -> &str {
         &self.id
     }
